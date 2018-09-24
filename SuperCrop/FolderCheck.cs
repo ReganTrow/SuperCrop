a042c3a9fd5fb4dd895727e46812d478c421c85a
Regan Trow <regan_trow@outlook.com>
2018-09-24 11:45:07 +1000

    Added image dimensions to dialog.

--- SuperCrop/FolderCheck.cs
+++ SuperCrop/FolderCheck.cs
@@ -4,6 +4,7 @@ using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Windows.Media.Imaging;
 
 namespace SuperCrop
 {
@@ -23,9 +24,31 @@ namespace SuperCrop
             }
         }
 
+        // Checks the folder path and returns how many .jpg files it contains. Will return 0 if the folder does not exist.
+        public static int GetJPGCount(string path)
+        {
+            if (Directory.Exists(path) == false)
+            {
+                return 0;
+            }
+            else
+            {
+                string[] filenameArray = Directory.GetFiles(path);
+                int jpgCounter = 0;
+                foreach (string filename in filenameArray)
+                {
+                    if (filename.EndsWith(".jpg"))
+                    {
+                        jpgCounter++;
+                    }
+                }
+                return jpgCounter;
+            }
+        }
+
 
         // Checks the folder path and returns how many files and jpg files it contains. Will return 0, 0 if the folder does not exist.
-        public static Tuple<int,int> GetJPGFileCount(string path)
+        public static Tuple<int,int> GetFileBreakdown(string path)
         {
             if (Directory.Exists(path)==false)
             {
@@ -44,6 +67,42 @@ namespace SuperCrop
                 }
                 return Tuple.Create(filenameArray.Length,jpgCounter);
             }
-        }        
+        }
+
+        
+
+        // Checks the folder path and returns a list with tuples of unique aspect ratios of images in the folder. 
+        // Will return an empty list if the folder does not exist.
+        public static List<Tuple<int,int>> GetAspectRatios(string path)
+        {
+            List<Tuple<int, int>> aspectRatioList = new List<Tuple<int, int>>();
+            if (Directory.Exists(path) == false)
+            {
+                return aspectRatioList;
+            }
+            else
+            {
+                string[] filenameArray = Directory.GetFiles(path);
+                foreach (string filename in filenameArray)
+                {
+                    if (filename.EndsWith(".jpg"))
+                    {
+                        BitmapDecoder imageDecoder = BitmapDecoder.Create(
+                            new Uri(filename, UriKind.Absolute),
+                            BitmapCreateOptions.DelayCreation,
+                            BitmapCacheOption.None);
+                            BitmapFrame imageFrame = imageDecoder.Frames[0];
+                        Tuple<int, int> imageDimensions = Tuple.Create(imageFrame.PixelHeight, imageFrame.PixelWidth);
+                        if (aspectRatioList.Contains(imageDimensions)==false)
+                        {
+                            aspectRatioList.Add(imageDimensions);
+                        }
+                    }
+                }
+                return aspectRatioList;
+            }
+        }
+
+
     }
 }
--- SuperCrop/FolderDescriptionHandling.cs
+++ SuperCrop/FolderDescriptionHandling.cs
@@ -17,7 +17,7 @@ namespace SuperCrop
             }
             else
             {
-                string descriptionString = "";
+                string descriptionString = "\n";
                 descriptionString += jpgCountText(path);
                 descriptionString += jpgAspectRatioText(path);
                 return descriptionString;
@@ -26,15 +26,27 @@ namespace SuperCrop
 
         private static string jpgCountText(string path)
         {
-            Tuple <int,int> fileCountTuple = FolderCheck.GetJPGFileCount(path);
-            return String.Format("There are {0} files in the specified folder, {1} of which are .jpg files. \n",
+            Tuple <int,int> fileCountTuple = FolderCheck.GetFileBreakdown(path);
+            return String.Format("There are {0} files in the specified folder, {1} of which are .jpg files.\n",
                 fileCountTuple.Item1,
                 fileCountTuple.Item2);
         }
 
         private static string jpgAspectRatioText(string path)
         {
-            return "";
+            if (FolderCheck.GetJPGCount(path)==0)
+            {
+                return "As there are no .jpg files in the specified cropping folder, no cropping operations will be possible.\n";
+            }
+            else
+            {
+                String outputString = "The following aspect ratios (H X W) are present in images in the specified cropping folder:\n";
+                foreach (var aspectRatio in FolderCheck.GetAspectRatios(path))
+                {
+                    outputString += String.Format("    - {0} x {1}\n", aspectRatio.Item1, aspectRatio.Item2);
+                }
+                return outputString;
+            }
         }
     }
 }
